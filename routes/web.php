bdb49329c46435cd29c9ff41a7556e1291c996cd
Ade Widyatama DB <41508539+adewidyatamadb@users.noreply.github.com>
2022-10-22 09:09:53 +0800

    Adding bootstrap and bootstrap auth scaffolding

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 });
 
 Route::resource('companies', \App\Http\Controllers\CompanyController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
