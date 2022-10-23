936b3f2e0e3d221537e0721be40485a8fa190237
Ade Widyatama DB <41508539+adewidyatamadb@users.noreply.github.com>
2022-10-23 21:52:50 +0800

    Adding mutator and accessor for name and address attribute

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -10,4 +10,21 @@ class Company extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'email', 'address'];
+
+    public function setNameAttribute($value){
+        $this->attributes['name'] = strtolower($value);
+    }
+
+    public function getNameAttribute($value): string
+    {
+        return strtoupper($value);
+    }
+
+    public function setAddressAttribute($value){
+        $this->attributes['address'] = strtolower($value);
+    }
+
+    public function getAddressAttribute($value): string{
+        return strtoupper($value);
+    }
 }
